e4a984bfe90ab0dd9d2fe6d09d29f973e4c043f2
Joshua Hawxwell <m@hawx.me>
2015-01-26 20:50:25 +0000

    Add method to get current user details

--- stream.go
+++ stream.go
@@ -2,6 +2,9 @@ package stream
 
 import (
 	"github.com/garyburd/go-oauth/oauth"
+
+	"net/http"
+	"encoding/json"
 	"log"
 )
 
@@ -9,11 +12,34 @@ const (
 	SAMPLE_URL   = "https://stream.twitter.com/1.1/statuses/sample.json"
 	STREAM_URL   = "https://userstream.twitter.com/1.1/user.json"
 	USER_URL     = STREAM_URL + "?with=user"
+
+	DETAILS_URL  = "https://api.twitter.com/1.1/account/verify_credentials.json"
 )
 
 type auth struct {
 	Oauth       *oauth.Client
 	Credentials *oauth.Credentials
+}
+
+func (a *auth) Details() (string, string, error) {
+	req, _ := http.NewRequest("GET", DETAILS_URL, nil)
+	req.Header.Set("Authorization", a.Oauth.AuthorizationHeader(a.Credentials, "GET", req.URL, nil))
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil || resp.StatusCode != 200 {
+		return "", "", err
+	}
+
+	var c struct {
+		Name string `json:"name"`
+		Url  string `json:"url"`
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&c); err != nil {
+		return "", "", nil
+	}
+
+	return c.Name, c.Url, nil
 }
 
 func Auth(consumerKey, consumerSecret, accessToken, accessSecret string) *auth {
